6f0a391a78d70efd613d237e172dadcfa100f2b2
Sekhar <sunkarisekhar36@gmail.com>
2024-03-26 13:04:35 +0530

    Changes Happend

--- CICheck.java
+++ CICheck.java
@@ -1,5 +1,6 @@
 public class CICheck {
     public static void main(String[] args) {
         System.out.println("This is Created for CI demo Checking purpose");
+        System.out.println("This is new line added and pushed Now");
     }
 }
